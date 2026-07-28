e0d2634403d4d8a6ae7034ddfa414eac6c23ed22
Nakano as a Service <nakanoasaservice@gmail.com>
2026-07-28 20:33:32 +0900

    feat: qrencode追加

--- flake.nix
+++ flake.nix
@@ -185,6 +185,7 @@
                   nixd
                   nixfmt
                   nushell
+                  qrencode
                   ripgrep
                   tldr
                   zellij
